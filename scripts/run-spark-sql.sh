22280f90a3d349863d42477229106fc427062420
Sandip Kumar Dey <sandipkumardey615@gmail.com>
2025-05-10 00:30:03 +0530

    feat: add config files and update gitignore for better file management

--- scripts/run-spark-sql.sh
+++ scripts/run-spark-sql.sh
@@ -5,7 +5,7 @@ spark-sql \
   --conf "spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions" \
   --conf "spark.sql.catalog.demo=org.apache.iceberg.spark.SparkCatalog" \
   --conf "spark.sql.catalog.demo.type=rest" \
-  --conf "spark.sql.catalog.demo.uri=http://iceberg-rest:8181" \
+  --conf "spark.sql.catalog.demo.uri=http://iceberg-rest-catalog:8181" \
   --conf "spark.sql.catalog.demo.warehouse=s3://warehouse" \
   --conf "spark.sql.catalog.demo.s3.endpoint=http://minio:9000" \
   --conf "spark.sql.catalog.demo.s3.access-key-id=minioadmin" \
